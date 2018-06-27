d6a19ab6d69d164c5b16f44c1dcfbe8d6cf27fdd
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 03:39:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
+  new_array=[]
   array.each do |number|
     blah = number*number
-    array.push(number*number)
+    array.push(blah)
   end
   
   # array.map { |n| n * n }
